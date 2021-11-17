9b76e47466b1351427338c9121471bdd605487ef
furawa <frankenprofessional@gmail.com>
2021-11-17 20:00:29 +0100

    feat: Allow the enter key to launch the search

--- assets/js/app.js
+++ assets/js/app.js
@@ -9,19 +9,21 @@ let keys = [];
 // FETCH FUNCTIONS
 //==========================================
 
-const fetchData = (url) => {
-        return fetch(url)
+const fetchData = async url => {
+        return  await fetch(url)
+                .then(checkStatus)
                 .then(res => res.json())
+                .catch(error => alert("Looks like there was a problem with this request!", error));
 }
 
-const fetchSubject = () => {
+function fetchSubject() {
     const subject = userInput.value;
     fetchData(`https://openlibrary.org/subjects/${subject}.json`)
         .then(data => generateResult(data.works));
     resetValues();
 }
 
-const fetchDescription = (key) => {
+const fetchDescription = key => {
     let desc = "";
     fetchData(`https://openlibrary.org${key}.json`)
         .then(data => {
@@ -36,6 +38,7 @@ const fetchDescription = (key) => {
             modalBody.innerHTML = `<p><span class="text-decoration-underline">Description</span><br><br>${desc}</p>`;
         });
 }
+
 //===============================================
 // HELPER FUNCTIONS
 //===============================================
@@ -58,6 +61,13 @@ function generateResult(data) {
     }
 }
 
+const checkStatus = response => {
+    if(response.ok) {
+        return Promise.resolve(response);
+    } else {
+        return Promise.reject(new Error(response.statusText));
+    }
+}
 
 //=======================================================
 // EVENT LISTENERS
@@ -65,8 +75,17 @@ function generateResult(data) {
 
 // Add event listener on the search button
 btnSearch.addEventListener("click", fetchSubject);
-// Add event on the card using bubbling and checking only button
 
+// Allow to search by using the Enter key
+userInput.addEventListener("keyup", event => {
+    if (event.key === "Enter") {
+        event.preventDefault();
+        btnSearch.click();
+    }
+})
+
+
+// Add event on the card using bubbling and checking only button
 card.addEventListener("click", (event) => {
     const r = /\d+/g;
     if(!(event.target.nodeName === "BUTTON")) {
@@ -74,6 +93,5 @@ card.addEventListener("click", (event) => {
     }
     const key = keys[Number(event.target.id.match(r)) - 1]; 
     fetchDescription(key);
-    document.getElementById("modalDesc").removeChild();
 })
 
