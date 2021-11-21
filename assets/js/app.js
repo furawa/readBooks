98d65d09b20c3110fcf46af8fe039bb83335a107
furawa <frankenprofessional@gmail.com>
2021-11-21 11:07:10 +0100

    refactor: Improve the responsivity of each card result

--- assets/js/app.js
+++ assets/js/app.js
@@ -57,9 +57,14 @@ function generateResult(data) {
     keys = []; // Empty the array
     for (let i = 0; i < data.length; i++) {
         const el = document.createElement("div");
-        el.className = "result col-8 col-sm-5 col-lg-3 p-0 d-flex justify-content-between border rounded-3 border-info";
+        if (i % 2 == 0) {
+            el.className = "result ms-sm-auto mx-md-auto col-8 col-md-5 col-lg-4 p-0 d-flex flex-column flex-sm-row border rounded-3 border-info";
+        } else {
+            el.className = "result me-sm-auto mx-md-auto col-8 col-md-5 col-lg-4 p-0 d-flex flex-column flex-sm-row border rounded-3 border-info";
+        }
+        
         const book = `<img src="https://covers.openlibrary.org/b/id/${data[i].cover_id}-M.jpg" alt="${data[i].title} Book cover">
-                      <div class="d-flex flex-column justify-content-between">
+                      <div class="d-flex flex-column flex-grow-1 justify-content-between">
                         <h2>Author</h2> <p>${data[i].authors[0].name}</p>
                         <h3>Title</h3><p>${data[i].title}</p>
                         <button data-bs-toggle="modal" data-bs-target="#modalDesc" type="button" class="align-self-center btn btnt bg-info" id=button${i + 1}>Learn More</button>
