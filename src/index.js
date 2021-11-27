a5d1d86473a03d76dc5d955d6f7587f59928d445
furawa <frankenprofessional@gmail.com>
2021-11-27 11:41:20 +0100

    refactor: Lazy load just bootstrap and js file

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
-import("./assets/js/event-listener");
-import("bootstrap-icons/font/bootstrap-icons.css");
+import "bootstrap-icons/font/bootstrap-icons.css";
 import("bootstrap");
-import("bootstrap/dist/css/bootstrap.min.css");
-import("./assets/css/style.css");
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./assets/css/style.css";
+import("./assets/js/event-listener");
+
